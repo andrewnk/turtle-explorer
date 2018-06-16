d44aab5497c05bd30f242f1f18c7d610a2513ca5
Andrew Kimball <andrewnk@comcast.net>
2018-06-16 09:22:07 -0400

    removed cache and config folders

--- web/frontend/.cache/yarn/v1/npm-@feathersjs/commons-1.4.1-81f19ffb83eac6afd33c629d4d0cad57441fbede/lib/commons.js
+++
@@ -1,6 +0,0 @@
-const utils = require('./utils');
-const hooks = require('./hooks');
-const args = require('./arguments');
-const filterQuery = require('./filter-query');
-
-module.exports = Object.assign({}, utils, args, { hooks, filterQuery });
