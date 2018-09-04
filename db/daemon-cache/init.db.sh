dd3993914329492534ba01c4c8547c5a84342e86
Andrew Kimball <andrewnk@comcast.net>
2018-09-03 21:57:36 -0400

    worked on daemon

--- db/daemon-cache/init.db.sh
+++ db/daemon-cache/init.db.sh
@@ -17,7 +17,7 @@ psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
         size BIGINT NOT NULL,
         fee BIGINT,
         amount_out BIGINT,
-        blockHash  VARCHAR(255) NOT NULL,
+        blockHash VARCHAR(255) NOT NULL,
         PRIMARY KEY(hash,paymentId,blockHash)
     );
 
@@ -36,26 +36,28 @@ psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     FOR EACH ROW EXECUTE PROCEDURE notify_transactions();
 
     CREATE TABLE IF NOT EXISTS blocks (
-      alreadyGeneratedCoins VARCHAR(255) NOT NULL,
-      alreadyGeneratedTransactions BIGINT NOT NULL,
-      baseReward BIGINT NOT NULL,
-      blockSize BIGINT NOT NULL,
+      already_generated_coins VARCHAR(255) NOT NULL,
+      already_generated_transactions BIGINT NOT NULL,
+      base_reward BIGINT NOT NULL,
+      block_size BIGINT NOT NULL,
+      depth BIGINT NOT NULL,
       difficulty BIGINT NOT NULL,
-      effectiveSizeMedian BIGINT NOT NULL,
+      effective_size_median BIGINT NOT NULL,
       hash VARCHAR(255) NOT NULL,
       height BIGINT NOT NULL,
       major_version BIGINT NOT NULL,
       minor_version BIGINT NOT NULL,
       nonce BIGINT NOT NULL,
-      orphan_status BIGINT NOT NULL,
+      orphan_status BOOLEAN NOT NULL,
       penalty BIGINT NOT NULL,
       prev_hash VARCHAR(255) NOT NULL,
       reward BIGINT NOT NULL,
-      sizeMedian BIGINT NOT NULL,
+      size_median BIGINT NOT NULL,
       timestamp BIGINT NOT NULL,
-      totalFeeAmount NUMERIC NOT NULL,
+      total_fee_amount NUMERIC NOT NULL,
       transactions LONGBLOB NOT NULL,
-      transactionsCumulativeSize BIGINT NOT NULL,
+      transactions_cumulative_size BIGINT NOT NULL,
+      status string
       PRIMARY KEY(height, hash)
     )
 
