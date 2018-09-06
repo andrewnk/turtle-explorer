44fd73529f302bcdcc572b192170572f03d1e98e
Andrew Kimball <andrewnk@comcast.net>
2018-09-06 13:40:49 -0400

    worked on status bar

--- web/frontend/layouts/sections/StatsBar.vue
+++ web/frontend/layouts/sections/StatsBar.vue
@@ -1,49 +1,80 @@
 <template>
     <section>
-        <b-collapse :open="true" class="is-centered">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>Height</th>
-                        <th>Difficulty</th>
-                        <th>Node Hashrate</th>
-                        <th>Pool Hashrate</th>
-                        <th>Miners</th>
-                        <th>Blocks</th>
-                        <th>Payments</th>
-                        <th>Miners Paid</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            {{ nodes.height.toLocaleString() }}
-                        </td>
-                        <td>
-                            {{ nodes.difficulty.toLocaleString() }}
-                        </td>
-                        <td>
-                            {{ humanReadableHashrate(nodes.hashrate) }}
-                        </td>
-                        <td>
-                            {{ poolHashrate !== null ? humanReadableHashrate(poolHashrate) : ''}}
-                        </td>
-                        <td>
-                            {{ poolMiners.toLocaleString() }}
-                        </td>
-                        <td>
-                            {{ poolBlocks.toLocaleString() }}
-                        </td>
-                        <td>
-                            {{ poolPayments.toLocaleString() }}
-                        </td>
-                        <td>
-                            {{ poolMinersPaid.toLocaleString() }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </b-collapse>
+        <div id="stats-container">
+            <transition-group tag="table" name="slide-fade">
+                <table class="table" key="table" id="stats-bar-table" v-if="show">
+                    <tbody>
+                        <tr>
+                            <th>Height</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ nodes.height.toLocaleString() }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Difficulty</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ nodes.difficulty.toLocaleString() }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Node Hashrate</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ humanReadableHashrate(nodes.hashrate) }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Pool Hashrate</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ poolHashrate !== null ? humanReadableHashrate(poolHashrate) : ''}}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Miners</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ poolMiners.toLocaleString() }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Blocks</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ poolBlocks.toLocaleString() }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Payments</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ poolPayments.toLocaleString() }}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>Miners Paid</th>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ poolMinersPaid.toLocaleString() }}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div id="toggle" key="div" @click="show = !show">
+                    <i class="fas" :class="fontClass"></i>
+                </div>
+            </transition-group>
+        </div>
     </section>
 </template>
 
@@ -54,6 +85,11 @@ import { mapGetters } from 'vuex'
 export default {
     name: 'StatsBar',
     mixins: [vueMixin],
+    data () {
+        return {
+            show: true
+        }
+    },
     computed: {
         ...mapGetters('node', { getNodes: 'list' }),
         ...mapGetters('pool', { getPools: 'list' }),
@@ -80,8 +116,11 @@ export default {
                 .reduce((acc, val) => {
                     return acc.hasOwnProperty('data') && acc.data.start_time > val.data.start_time ? acc.data : val.data
                 })
+        },
+        fontClass() {
+            return this.show ? 'fa-angle-double-left' : 'fa-angle-double-right'
         }
-    },
+    }
 }
 
 </script>
