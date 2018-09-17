4eb311ea8ce5f58a84d2feffd71a638b225330cf
Andrew Kimball <andrewnk@comcast.net>
2018-09-17 16:58:47 -0400

    fixed pool historical graph initial load value

--- web/frontend/components/pools/graphs/PoolHistory.vue
+++ web/frontend/components/pools/graphs/PoolHistory.vue
@@ -110,7 +110,7 @@ export default {
                         $gte: '',
                         $lte: ''
                     },
-                    attribute: 'miners'
+                    attribute: 'difficulty'
                 }
             },
             attributes: [
