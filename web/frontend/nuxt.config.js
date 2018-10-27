9c202b0542e9e161cdcf22cb314a8ad29b552798
Andrew Kimball <andrewnk@comcast.net>
2018-10-27 12:58:36 -0400

    set global meta tags

--- web/frontend/nuxt.config.js
+++ web/frontend/nuxt.config.js
@@ -46,7 +46,8 @@ module.exports = {
     title: 'TurtleCoin Explorer',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'description', content: 'Explore historical and live block, node, pool, and transaction data for TurtleCoin' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
